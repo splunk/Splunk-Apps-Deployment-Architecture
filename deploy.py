7c0b03d990c49fd547c6d3743304e96a1f493aa6
bdebek-splunk <bdebek@splunk.com>
2024-12-10 11:42:37 +0100

    fix: Update comments in deploy.py for clarity and organization

--- deploy.py
+++ deploy.py
@@ -170,7 +170,7 @@ def main():
         print(f"Error parsing YAML file: {e}")
 
     # TODO NEXT
-    # 1. Retrieve all apps listed in deployment.yml from S3
+    ### 1. Retrieve all apps listed in deployment.yml from S3 ###
 
     # List all apps in yaml file and then their S3 bucket
     apps = [app for app in data['apps']]
@@ -184,12 +184,14 @@ def main():
         file_name = f"{app}.tgz"
         # Donwload app from S3
         download_file_from_s3(bucket, object_name, file_name)
-        # Upload_local_configurateion
+
+    ### 2. Upload_local_configurateion ###
         # if os.path.exists(f"environments/{sys.argv[1]}/{app}"):
         #     unpack_load_conf_and_repack(app)
         # else:
         #     print(f"No configuration found for app {app}. Skipping.")
-        # Validate app for Splunk Cloud
+
+    ### 3. Validate app for Splunk Cloud ###
         raport, token = cloud_validate_app(app, SPLUNK_CLOUD_CONFIG)
         if raport is None:
             print(f"App {app} failed validation.")
@@ -197,7 +199,7 @@ def main():
             continue
         result = raport['summary']
         deployment_raport[app] = raport
-        # If app is valid, distribute it
+    ### 4. If app is valid, distribute it ###
         if result['error'] == 0 and result['failure'] == 0 and result['manual_check'] == 0:
             distribution_status = distribute_app(app, target_url, token)
             if distribution_status == 200:
@@ -210,7 +212,7 @@ def main():
             print(f"App {app} failed validation. Skipping distribution.")
             deployment_raport[app]['distribution'] = 'failed due to app validation error'
 
-    # Save deployment raport to json file
+    ### 5. Save deployment raport to json file ###
     raport_prefix = f"{sys.argv[1].split('/')[-2]}_{sys.argv[1].split('/')[-1]}"
     output_dir = "artifacts"
     os.makedirs(output_dir, exist_ok=True)
